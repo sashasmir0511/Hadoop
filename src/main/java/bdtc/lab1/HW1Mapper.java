23db1c9003eab2b113e49f2a9b72aaa9f689991f
root <root@localhost.localdomain>
2021-04-09 20:28:59 -0400

    -comment

--- src/main/java/bdtc/lab1/HW1Mapper.java
+++ src/main/java/bdtc/lab1/HW1Mapper.java
@@ -12,15 +12,8 @@ import java.util.regex.Pattern;
 
 
 public class HW1Mapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-    private final static IntWritable X = new IntWritable(1);
-    private final static IntWritable Y = new IntWritable(1);
-    private final static IntWritable X_1 = new IntWritable(1);
-    private final static IntWritable Y_1 = new IntWritable(1);
-    private final static IntWritable X_2 = new IntWritable(1);
-    private final static IntWritable Y_2 = new IntWritable(1);
+
     private final static IntWritable one = new IntWritable(1);
-    private static final String[] values_key =
-            {"0:0:300:50:bot", "0:50:300:150:body", "0:150:300:200:top"};
     private Text word = new Text();
 
     @Override
@@ -51,57 +44,4 @@ public class HW1Mapper extends Mapper<LongWritable, Text, Text, IntWritable> {
             context.getCounter(CounterType.MALFORMED).increment(1);
         }
     }
-
-    /*@Override
-    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String line = value.toString();
-
-        if (Pattern.matches("^[0-9]{3} [0-9]{3} [0-9]{3} [0-9]{10}$",line)) {
-            String[] ml = line.split(" ");
-            String[] key_bot = values_key[0].split(":");
-            String[] key_body = values_key[1].split(":");
-            String[] key_top = values_key[2].split(":");
-
-            if (!ml[0].isEmpty() && !ml[1].isEmpty()) {
-                X.set(Integer.parseInt(ml[0]));
-                Y.set(Integer.parseInt(ml[1]));
-                X_1.set(Integer.parseInt(key_bot[0]));
-                X_2.set(Integer.parseInt(key_bot[2]));
-                Y_1.set(Integer.parseInt(key_bot[1]));
-                Y_2.set(Integer.parseInt(key_bot[3]));
-
-                if (X.get() >= X_1.get() && X.get() < X_2.get()
-                        && Y.get() >= Y_1.get() && Y.get() < Y_2.get()) {
-                    word.set(ml[2]);
-                    context.write(word, new IntWritable(0));
-                    return;
-                }
-                X_1.set(Integer.parseInt(key_body[0]));
-                X_2.set(Integer.parseInt(key_body[2]));
-                Y_1.set(Integer.parseInt(key_body[1]));
-                Y_2.set(Integer.parseInt(key_body[3]));
-
-                if (X.get() >= X_1.get() && X.get() < X_2.get()
-                        && Y.get() >= Y_1.get() && Y.get() < Y_2.get()) {
-                    word.set(ml[2]);
-                    context.write(word, new IntWritable(1));
-                    return;
-                }
-                X_1.set(Integer.parseInt(key_top[0]));
-                X_2.set(Integer.parseInt(key_top[2]));
-                Y_1.set(Integer.parseInt(key_top[1]));
-                Y_2.set(Integer.parseInt(key_top[3]));
-
-                if (X.get() >= X_1.get() && X.get() < X_2.get()
-                        && Y.get() >= Y_1.get() && Y.get() < Y_2.get()) {
-                    word.set(ml[2]);
-                    context.write(word, new IntWritable(2));
-                    return;
-                }
-            }
-        } else {
-            System.out.println("Zero");
-            context.getCounter(CounterType.MALFORMED).increment(1);
-        }
-    }*/
 }
--- src/main/java/bdtc/lab1/HW1Reducer.java
+++ src/main/java/bdtc/lab1/HW1Reducer.java
@@ -11,35 +11,7 @@ import java.io.IOException;
  * Редьюсер: суммирует все единицы полученные от {@link HW1Mapper}, выдаёт суммарное количество пользователей по браузерам
  */
 public class HW1Reducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-    /*private static Text keyWritable = new Text();
-    private static MapWritable dictWritable = new MapWritable();
-    private static String[] values_key =
-            {"bot", "body", "top"};
 
-     */
-
-    /*@Override
-    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-        keyWritable = new Text();
-        dictWritable = new MapWritable();
-
-        while (values.iterator().hasNext()){
-            int element = values.iterator().next().get();
-            IntWritable valueWritable = new IntWritable();
-            keyWritable.set(new Text(values_key[element]));
-            if (dictWritable.get(keyWritable) != null) {
-                valueWritable = (IntWritable) dictWritable.get(keyWritable);
-                valueWritable.set(valueWritable.get() + 1);
-
-                dictWritable.put(new Text(keyWritable), valueWritable);
-            }
-            else {
-                valueWritable.set(1);
-                dictWritable.put(new Text(keyWritable), valueWritable);
-            }
-        }
-        context.write(key,  dictWritable);
-    }*/
     @Override
     protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
         int sum = 0;
